e9c49b3de585b34047c8a104a0fbe809680bc000
Nikola Mihajlovic <nikola.mihajlovic@ict.edu.rs>
2018-11-03 17:27:42 +0100

    Template izgleda kako treba

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,8 +8,11 @@ import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { SidenavComponent } from './sidenav/sidenav.component';
 import { ChatWindowComponent } from './chat/chat-window/chat-window.component';
-import { MainComponent } from './profile/main/main/main.component';
+import { MainComponent } from './profile/main/main.component';
 import { ProfileComponent } from './profile/profile.component';
+import { RightColumnComponent } from './profile/main/right-column/right-column.component';
+import { LeftColumnComponent } from './profile/main/left-column/left-column.component';
+import { FooterComponent } from './profile/main/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +22,10 @@ import { ProfileComponent } from './profile/profile.component';
     SidenavComponent,
     ChatWindowComponent,
     MainComponent,
-    ProfileComponent
+    ProfileComponent,
+    RightColumnComponent,
+    LeftColumnComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
